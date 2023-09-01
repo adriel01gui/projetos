439fc0805280e2c95d45f4d395c4b0ce87b62e8d
Adriel Guimaraes <adrielguimaraes01@gmail.com>
2023-09-01 11:53:26 -0300

    criação de entidades

---
+++ backVendas/src/main/java/com/java/backVendas/models/ItensVenda.java
@@ -0,0 +1,29 @@
+package com.java.backVendas.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItensVenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_venda")
+    private Venda venda;
+
+    @Column(name = "quantidade")
+    private Double quantidade;
+}
